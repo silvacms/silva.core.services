3d599acf73d822ebc9d19e0d7eddfc7dd54fd5e7
Sylvain Viollon <thefunny@gmail.com>
2010-06-29 13:13:48 +0000

    walk_silva_tree can return object version with it.

--- silva/core/services/utils.py
+++ silva/core/services/utils.py
@@ -6,7 +6,7 @@ from silva.core import interfaces
 
 THRESHOLD = 1000
 
-def walk_silva_tree(content):
+def walk_silva_tree(content, version=False):
     """A generator to lazily get all the objects that need to be
     indexed.
         """
@@ -14,12 +14,14 @@ def walk_silva_tree(content):
         # Version are indexed by the versioned content itself
         yield content
     count = 0
-    if interfaces.IContainer.providedBy(content):
+    if (interfaces.IContainer.providedBy(content)):
         for child in content.objectValues():
-            for content in walk_silva_tree(child):
+            for content in walk_silva_tree(child, version=version):
                 count += 1
                 yield content
                 if count > THRESHOLD:
                     # Review ZODB cache
                     content._p_jar.cacheGC()
                     count = 0
+    if version and interfaces.IVersionedContent.providedBy(content):
+        yield content.get_previewable()
