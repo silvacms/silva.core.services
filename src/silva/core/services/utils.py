f011c3fcc107afabfc5e3bfd8913fc23c490d320
Sylvain Viollon <thefunny@gmail.com>
2011-03-28 14:51:04 +0200

    Add a more advanced iterator.

--- src/silva/core/services/utils.py
+++ src/silva/core/services/utils.py
@@ -2,19 +2,19 @@
 # See also LICENSE.txt
 # $Id$
 
-from silva.core import interfaces
+from silva.core.interfaces import ISilvaObject, IContainer, IVersionedContent
 
 THRESHOLD = 1000
 
 def walk_silva_tree(content, version=False):
-    """A generator to lazily get all the objects that need to be
-    indexed.
-        """
-    if interfaces.ISilvaObject.providedBy(content):
+    """A generator to lazily get all the Silva object from a content /
+    container.
+    """
+    count = 0
+    if ISilvaObject.providedBy(content):
         # Version are indexed by the versioned content itself
         yield content
-    count = 0
-    if (interfaces.IContainer.providedBy(content)):
+    if IContainer.providedBy(content):
         for child in content.objectValues():
             for content in walk_silva_tree(child, version=version):
                 count += 1
@@ -23,5 +23,33 @@ def walk_silva_tree(content, version=False):
                     # Review ZODB cache
                     content._p_jar.cacheGC()
                     count = 0
-    if version and interfaces.IVersionedContent.providedBy(content):
+    if version and IVersionedContent.providedBy(content):
         yield content.get_previewable()
+
+
+# XXX need testing.
+def advanced_walk_silva_tree(content, requires=ISilvaObject, version=False):
+    """A controllable generator to lazily get all the Silva object
+    from a content / container. Send it True to recursively go down,
+    or False to skip the recursion.
+    """
+    count = 0
+    want_next = True
+    if requires.providedBy(content):
+        want_next = yield content
+    if IContainer.providedBy(content) and want_next:
+        for child in content.objectValues():
+            walker_next = None
+            walker = advanced_walk_silva_tree(child, requires, version)
+            while True:
+                count += 1
+                try:
+                    walker_next = yield walker.send(walker_next)
+                except StopIteration:
+                    break
+                if count > THRESHOLD:
+                    # Review ZODB cache
+                    content._p_jar.cacheGC()
+                    count = 0
+    if version and IVersionedContent.providedBy(content):
+        want_next = yield content.get_previewable()
